2e2565edec01493acde0eb9ed081bba991d624ca
Maxi <maxi_rosanda@hotmail.com>
2021-02-02 10:15:30 -0300

    segundo

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Card, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
-const Item = ({ item }) => {
+const Item = ({ key , item }) => {
 
   return (
     <React.Fragment>
